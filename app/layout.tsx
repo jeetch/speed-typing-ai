c321fc4e11df815c939bd075fec184e23068eac6
jeetch <jeetch96@gmail.com>
2024-05-06 22:48:22 +0530

    edited readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google"; // Import Mono font
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const mono = Roboto_Mono({ subsets: ["latin"] }); // Use Mono font
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={mono.className}>{children}</body> // Use Mono font here
     </html>
   );
 }
